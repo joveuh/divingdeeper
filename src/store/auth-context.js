e3fa2f7177fa5ff61dc6925b6d2e7277060044bf
Uzair H <uzair@ibm.com>
2022-09-30 14:43:51 -0400

    add the empty function declaration inside the context component so that IDE is able to pick it up

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const AuthContext = React.createContext({ isLoggedIn: false });
+const AuthContext = React.createContext({ 
+    isLoggedIn: false,
+    onLogout: (() => {})
+
+});
 
 export default AuthContext;
